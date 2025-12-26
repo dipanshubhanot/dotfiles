2b01e4c11daf5a8980eeb10550ba84b228ecba7e
Deepanshu Bhanot <dipanshubhanot@gmail.com>
2025-12-27 05:02:10 +1100

    Added a right prompt to starship

--- bash/fzf_utils.sh
+++ bash/fzf_utils.sh
@@ -5,8 +5,22 @@
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND='fd --type f'
 export FZF_ALT_C_COMMAND='fd --type d'
-export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border"
+# export FZF_DEFAULT_OPTS="--height 40% --layout=reverse"
+export FZF_DEFAULT_OPTS=" \
+--height 40% \
+--layout=reverse \
+--pointer='->' \
+--marker='+' \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+#
+# UTILITY FUNCINS DERIVED FROM FZF
+#
+# -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # 1. GIT: Checkout Branch or Tag
 # -----------------------------------------------------------------------------
